421bf2a67f201519c49c4f050dcac9307a7624ff
groob <vrancean+github@gmail.com>
2014-07-14 14:04:07 -0400

    added more preferences to testvm

--- manifests/osxtestvm.pp
+++ manifests/osxtestvm.pp
@@ -1,5 +1,11 @@
 class role::osxtestvm  {
   include profile::mcx_profile::login_window
   include profile::mcx_profile::safari_homepage
+  include profile::mcx_profile::managed_installs
+  include profile::mcx_profile::sal_preferences
+  include profile::network_settings::ipv6off
+  include profile::network_settings::wifi_proxy
+  include profile::network_settings::softwareupdate_schedule
+
 }
 
