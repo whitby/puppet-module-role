e6e1bc3e1b805e57c79149929e026973d8fb2dce
groob <vrancean+github@gmail.com>
2014-07-15 11:10:55 -0400

    fixed typo

--- manifests/osxtestvm.pp
+++ manifests/osxtestvm.pp
@@ -3,7 +3,7 @@ class role::osxtestvm  {
   include profile::mcx_profile::safari_homepage
   include profile::mcx_profile::managed_installs
   include profile::mcx_profile::sal_preferences
-  include profile::mcx_profile::suppress_icould_asst
+  include profile::mcx_profile::suppress_icloud_asst
   include profile::mac_settings::ipv6off
   include profile::mac_settings::wifi_proxy
   include profile::mac_settings::softwareupdate_schedule
